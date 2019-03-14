41d527658a796c3869c925bb1ef5a14929d8be13
Andrey Leskov <andrey.leskov@gmail.com>
2019-03-14 22:58:13 +0800

    Remove classes from single file

--- MightyCalc.API/MightyCalc.Calculations/ICalculator.cs
+++ MightyCalc.API/MightyCalc.Calculations/ICalculator.cs
@@ -9,36 +9,5 @@ namespace MightyCalc.Calculations
 
         IReadOnlyList<FunctionDefinition> GetKnownFunctions();
     }
-
-    public class CalculationResult
-    {
-        public CalculationResult(double value, IReadOnlyCollection<string> functionUsages)
-        {
-            Value = value;
-            FunctionUsages = functionUsages;
-        }
-
-        public IReadOnlyCollection<string> FunctionUsages { get; }
-        public double Value { get; }
-    }
-
-
-   
-    public class FunctionDefinition
-    {
-        public FunctionDefinition(string name, int arity, string description, string expression, params string [] parameters)
-        {
-            Name = name;
-            Arity = arity;
-            Description = description;
-            Expression = expression;
-            Parameters = parameters;
-        }
-
-        public string Name { get; }
-        public string Description { get; }
-        public int Arity { get; }
-        public string Expression { get; }
-        public string[] Parameters { get; }
-    }
+    
 }
