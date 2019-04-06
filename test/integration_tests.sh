093fa98bae41196937d2b74fbd7a880a8ad77e8c
Andrey Leskov <andrey.leskov@gmail.com>
2019-04-06 17:56:21 +0800

    Remove integration tests detailed output from build output.

--- test/integration_tests.sh
+++ test/integration_tests.sh
@@ -6,10 +6,10 @@ kubectl apply -f mightycalc-tests.yaml
 ./wait_pod_is_ready.sh 60 1
 
 echo "Starting node integration tests"
-kubectl exec -it mightycalc-test -- dotnet test ./MightyCalc.Node.IntegrationTests/MightyCalc.Node.IntegrationTests.csproj -c Release --no-build | tee node_integration_tests.txt
+kubectl exec -it mightycalc-test -- dotnet test ./MightyCalc.Node.IntegrationTests/MightyCalc.Node.IntegrationTests.csproj -c Release --no-build > node_integration_tests.txt
 
 echo "Starting report integration tests"
-kubectl exec -it mightycalc-test -- dotnet test ./MightyCalc.Reports.IntegrationTests/MightyCalc.Reports.IntegrationTests.csproj -c Release --no-build | tee report_integration_tests.txt
+kubectl exec -it mightycalc-test -- dotnet test ./MightyCalc.Reports.IntegrationTests/MightyCalc.Reports.IntegrationTests.csproj -c Release --no-build > report_integration_tests.txt
 
 echo "Starting api integration tests"
-kubectl exec -it mightycalc-test -- dotnet test ./MightyCalc.API.IntegrationTests/MightyCalc.API.IntegrationTests.csproj -c Release --no-build | tee api_integration_tests.txt
+kubectl exec -it mightycalc-test -- dotnet test ./MightyCalc.API.IntegrationTests/MightyCalc.API.IntegrationTests.csproj -c Release --no-build > api_integration_tests.txt
