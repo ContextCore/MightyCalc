8c5b2f0a9469529b3d8b5663661c93b1e474f8c9
Andrey Leskov <andrey.leskov@gmail.com>
2019-04-06 17:41:30 +0800

    Use docker desktop version of external service for mighty calc for minukube environment

--- deploy/create_environment.sh
+++ deploy/create_environment.sh
@@ -4,8 +4,13 @@ k8sProvider=$1 #gke or dd (docker-desktop) or mk (minikube)
 
 kubectl apply -f ./api/mightycalc-api-deploy.yaml
 kubectl apply -f ./api/mightycalc-api-svc.yaml
-kubectl apply -f ./api/mightycalc-api-svc-external-${k8sProvider}.yaml
 
+if [ $k8sProvider = "dd" ] || [ $k8sProvider = "mk" ]
+then
+kubectl apply -f ./api/mightycalc-api-svc-external-dd.yaml
+else 
+kubectl apply -f ./api/mightycalc-api-svc-external-gke.yaml
+fi
 kubectl apply -f ./persistence/storageclass-${k8sProvider}.yaml
 #kubectl apply -f ./persistence/persistence-volumeclaim.yaml
 kubectl apply -f ./persistence/persistence-deploy.yaml
