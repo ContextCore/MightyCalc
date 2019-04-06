f285d0fbb05e4f9fe5394835de6ef1e74df01ea2
Andrey Leskov <andrey.leskov@gmail.com>
2019-04-06 16:22:22 +0800

    Add minikube-specific storage class to be used on appveyor

--- deploy/create_environment.sh
+++ deploy/create_environment.sh
@@ -1,15 +1,17 @@
 set -Eeuxo pipefail
 # this script will create an environment on target Kubernetes cluster
-k8sProvider=$1 #gke or dd (docker-desktop)
+k8sProvider=$1 #gke or dd (docker-desktop) or mk (minikube)
 
 kubectl apply -f ./api/mightycalc-api-deploy.yaml
 kubectl apply -f ./api/mightycalc-api-svc.yaml
 kubectl apply -f ./api/mightycalc-api-svc-external-${k8sProvider}.yaml
 
+kubectl apply -f ./persistence/storageclass-${k8sProvider}.yaml
+#kubectl apply -f ./persistence/persistence-volumeclaim.yaml
 kubectl apply -f ./persistence/persistence-deploy.yaml
 kubectl apply -f ./persistence/persistence-svc.yaml
+
 if [ $k8sProvider = "dd" ]
 then
 kubectl apply -f ./persistence/persistence-svc-external.yaml
 fi
-kubectl apply -f ./persistence/storageclass-${k8sProvider}.yaml
