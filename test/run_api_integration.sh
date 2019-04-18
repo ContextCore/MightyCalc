3e49befcadda544a3322e2f1d4715376c0b748ab
Andrey Leskov <andrey.leskov@gmail.com>
2019-04-18 14:57:37 +0800

    gather logs from postgres pod

--- test/run_api_integration.sh
+++ test/run_api_integration.sh
@@ -6,6 +6,8 @@ workerPodName=$(get_pod worker)
 kubectl exec seed-0  -- pbm localhost:9110 cluster tail > seed_cluster_events.txt &
 kubectl logs -f seed-0  > seed_pod_logs.txt &
 
+kubectl logs -f postgres-0  > postgres_pod_logs.txt &
+
 kubectl exec $workerPodName -- pbm localhost:9110 cluster tail > worker_cluster_events.txt &
 kubectl logs -f $workerPodName  > worker_pod_logs.txt &
 
@@ -20,3 +22,4 @@ zip -r "api_cluster_events_logs.zip"  ./api_cluster_events.txt
 zip -r "seed_pod_logs.zip"  ./seed_pod_logs.txt
 zip -r "worker_pod_logs.zip"  ./worker_pod_logs.txt
 zip -r "api_pod_logs.zip"  ./api_pod_logs.txt
+zip -r "postgres_pod_logs.zip"  ./postgres_pod_logs.txt
